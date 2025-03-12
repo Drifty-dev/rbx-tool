46521066a1680b52d3896c8c87bcd6db9776c57b
Drifty-dev <driftyloltestlol@gmail.com>
2025-03-11 19:45:43 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@ document.addEventListener("DOMContentLoaded", function() {
         const v7w8x9 = {
             icon: "info",
             title: "<b>Is this correct? 👀</b>",
-            html: "Follow Amount: " + s4t5u6,
+            html: "Follow Amount: " + s4t5u7,
             showDenyButton: true,
             confirmButtonText: "Confirm", 
             denyButtonText: "Cancel",
